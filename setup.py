4678bb2ad5b4c2b60b3528f09c629bfd1cb412d1
Anton Pechenko <avp@giant.ai>
2021-07-09 02:54:12 +0300

    added package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='nflib',
+      version='1.0',
+      description='Normalizing flows library',
+      author='Andrey Karpathy',
+      author_email='',
+      url='https://github.com/karpathy/pytorch-normalizing-flows',
+      packages=['nflib'],
+     )
